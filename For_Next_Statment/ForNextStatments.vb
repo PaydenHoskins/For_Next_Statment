94276eb55b5dce411793371fe377d9fc37ae1c1e
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-29 08:56:09 -0700

    tried converting strings to diffrent formats

--- For_Next_Statment/ForNextStatments.vb
+++ For_Next_Statment/ForNextStatments.vb
@@ -9,6 +9,7 @@ Option Strict On
 Module ForNextStatments
 
     Sub Main()
+        Dim Result As String
         For i As Integer = 1 To 10 Step 1
             Console.WriteLine(i)
         Next
@@ -26,6 +27,28 @@ Module ForNextStatments
         Next
 
         Console.WriteLine(StrDup(80, "*"))
+
+        'Nested For Loop
+
+        For i = 1 To 10
+            Console.WriteLine($"{i}. ")
+            For j = 1 To 10
+
+                'Result = (Str(i + j))
+                'Result = Result.PadLeft(3)
+                'Console.Write(Result)
+
+                'Console.Write($"{i + j}".PadLeft(3) & "|")
+
+                Result = (j + i).ToString("")
+                Result = Result.PadLeft(8)
+                Console.Write(Result)
+            Next
+            Console.WriteLine()
+
+        Next
+
+
     End Sub
 
 End Module
