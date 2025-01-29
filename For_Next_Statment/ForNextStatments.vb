f554e196a280bcfbf9cfad2bdd322d4577b3c91c
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-29 08:19:48 -0700

    added more for structures and line breaks with stars

--- For_Next_Statment/ForNextStatments.vb
+++ For_Next_Statment/ForNextStatments.vb
@@ -9,9 +9,23 @@ Option Strict On
 Module ForNextStatments
 
     Sub Main()
+        For i As Integer = 1 To 10 Step 1
+            Console.WriteLine(i)
+        Next
+
+        Console.WriteLine(StrDup(80, "*"))
+
         For i = 1 To 10 Step 0.5
             Console.WriteLine(i)
         Next
+
+        Console.WriteLine(StrDup(80, "*"))
+
+        For i = 1 To 10
+            Console.WriteLine(i)
+        Next
+
+        Console.WriteLine(StrDup(80, "*"))
     End Sub
 
 End Module
