d3f547d08c478690bf78c017ece0e1b5600203da
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-29 08:11:38 -0700

    Add project files.

---
+++ For_Next_Statment/ForNextStatments.vb
@@ -0,0 +1,17 @@
+﻿'Payden Hoskins
+'Spring 2025
+'RCET2265
+'OperatorsExample
+
+Option Explicit On
+Option Strict On
+
+Module ForNextStatments
+
+    Sub Main()
+        For i As Integer = 1 To 10 Step 1
+            'Do Stuff
+        Next
+    End Sub
+
+End Module
---
+++ For_Next_Statment/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("For_Next_Statment")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("For_Next_Statment")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("233847fc-d880-4a51-9d92-dc9dcc2ec507")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
